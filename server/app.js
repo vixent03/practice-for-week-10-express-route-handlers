4a46b8638ab85618193048ca3a559e790ae9f381
vi xent <alagovincent03@gmailcom>
2023-08-06 18:14:06 -0700

    done

--- server/app.js
+++ server/app.js
@@ -23,8 +23,23 @@ const {
 
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Body:', req.body);
+  next();
+});
 
 // Your code here
+
+app.get("/artists",(req,res)=>{
+  res.send(JSON.stringify(getAllArtists()))
+})
+
+app.post("/artists",(req,res)=>{
+  addArtist(req.body)
+  res.status(201)
+  res.send(JSON.stringify(req.body))
+})
 
 // DO NOT MODIFY
 if (require.main === module) {
